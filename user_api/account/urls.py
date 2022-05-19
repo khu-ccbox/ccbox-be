81984629ed88f9f4200ebe3d387371b5ae8d2e9a
SeoroMin <57586314+SeoroMin@users.noreply.github.com>
2022-05-19 17:52:30 +0900

    Delete user_api directory

--- user_api/account/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from .view import AccountView, SignView
-
-urlpatterns = [
-    path('', AccountView.as_view()),
-    path('/sign-up', AccountView.as_view()),
-    path('/sign-in', SignView.as_view()),
-]
